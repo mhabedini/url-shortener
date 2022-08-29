bd487af481bbfa80e107a87d91d95558d8ab351a
m.h.a.abedini@gmail.com <mohammadhoseinabedini@gmail.com>
2022-08-29 20:26:51 +0430

    Refactor router

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,14 +1,15 @@
 <?php
 
-require_once dirname(__FILE__) . '/../vendor/autoload.php';
-
-$config = require  dirname(__FILE__) .'/../config/app.php';
-$database = require dirname(__FILE__) . '/../config/database.php';
-
 use Filimo\UrlShortener\Database\Connection;
 use Filimo\UrlShortener\Database\Query\Builder;
+use Filimo\UrlShortener\Support\Http\Router;
 
-$pdo = Connection::make($database);
-$query = new Builder($pdo);
+
+require_once dirname(__FILE__) . '/../vendor/autoload.php';
+require dirname(__FILE__) . '/../config/database.php';
+Router::load('api.php', 'api');
+
+// $pdo = Connection::make($database);
+// $query = new Builder($pdo);
 
 return null;
--- src/Support/Http/Router.php
+++ src/Support/Http/Router.php
@@ -9,6 +9,7 @@ class Router
 
     protected static ?Router $instance = null;
     protected array $routes = [];
+    protected string $prefix = '';
 
     private function __construct()
     {
@@ -23,19 +24,28 @@ class Router
         return static::$instance;
     }
 
+    public static function load(string $file, $prefix = ''): static
+    {
+        $router = self::getInstance();
+        $router->prefix = $prefix;
+        require_once dirname(__FILE__) . "/../../../routes/$file";
+        return $router;
+    }
+
     public function define(string $address, array $controller, HttpMethod $method): void
     {
-        $this->routes[$method->value][trim($address, '/')] = $controller;
+        $this->routes[$method->value][$this->prefix . '/' . trim($address, '/')] = $controller;
     }
 
     /**
      * @throws Exception
      */
-    public function direct(string $uri, HttpMethod $method)
+    public static function direct(string $uri, HttpMethod $method)
     {
-        if (array_key_exists($method->value, $this->routes)) {
-            if (array_key_exists($uri, $this->routes[$method->value])) {
-                return $this->routes[$method->value][$uri];
+        $router = self::getInstance();
+        if (array_key_exists($method->value, $router->routes)) {
+            if (array_key_exists($uri, $router->routes[$method->value])) {
+                return $router->routes[$method->value][$uri];
             }
         }
         throw new Exception('Not route defined for this URI');
